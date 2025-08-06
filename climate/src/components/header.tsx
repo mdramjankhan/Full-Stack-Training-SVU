3be9a883ea196ffcba37a4ce087d61e1487a6a26
mdramjankhan <rk9032929@gmail.com>
2025-08-06 15:40:36 +0530

    added the theme changing ui in header

--- climate/src/components/header.tsx
+++ climate/src/components/header.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "@/context/theme-provide"
+import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
@@ -9,9 +10,25 @@ export default function Header() {
         <div className="container mx-auto flex items-center h-16 justify-between px-4">
             <Link to='/'>
                 <img src={theme == 'dark' ? '/logo.png' : '/logo2.png'} alt="climate-logo"
-                 className=""
+                 className="h-8 w-8"
                 />
             </Link>
+        </div>
+
+        <div className={`flex items-center cursor-pointer ${isDark ? 'text-white' : 'text-black'} 
+            transition-transform duration-500 ease-in-out ${isDark ? 'rotate-180' : 'rotate-180'}
+            
+        `}
+            onClick={() => setTheme(isDark ? 'light' : 'dark')}
+        >
+            {
+                isDark?(
+                    <Sun onClick={() => setTheme('light')} className="h-6 w-6 text-yellow-500 rotate-180 transition-all"></Sun>
+                ):(
+                    <Moon onClick={() => setTheme('dark')} className="h-6 w-6 text-blue-500 rotate-180 transition-all"></Moon>
+                )
+            }
+
         </div>
     </header>
   )
