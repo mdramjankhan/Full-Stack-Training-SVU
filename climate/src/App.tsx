49224d7d85cde95d2391700f438652f35bdf3e5d
mdramjankhan <rk9032929@gmail.com>
2025-08-07 15:21:16 +0530

    Added more info and new files in the climate code in Class

--- climate/src/App.tsx
+++ climate/src/App.tsx
@@ -4,11 +4,17 @@ import { Layout } from './components/layout';
 import { ThemeProvider } from './context/theme-provide';
 import { WeatherDashboard } from './pages/weather-dashboard';
 import { CityPage } from './pages/city-page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <ThemeProvider defaultTheme='dark'>
           <Layout>
@@ -19,6 +25,8 @@ function App() {
           </Layout>
         </ThemeProvider>
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   )
 }
